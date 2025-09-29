10f6d292491eba91c4cb8309adad0a237a9dedff
Nabilah Nuraini Rohadatul 'Aisy <nabilahnuraisy@gmail.com>
2025-09-29 17:53:50 +0700

    Implement Employee CRUD basic structure (Controller, View, Routes)

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    public function index()
+    {
+        $employees = Employee::latest()->paginate(5);
+        
+        return view('employees.index', compact('employees'));
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
