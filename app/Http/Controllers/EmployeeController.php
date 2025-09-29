aad352f25569e2325acead64be30f4f94afa58d7
Nabilah Nuraini Rohadatul 'Aisy <nabilahnuraisy@gmail.com>
2025-09-29 18:46:22 +0700

    Implement employee edit form and update logic

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -43,12 +43,33 @@ class EmployeeController extends Controller
 
     public function edit(string $id)
     {
-        //
+        $employee = Employee::find($id);
+        
+        return view('employees.edit',compact('employee'));
     }
 
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+        'nama_lengkap'  => 'required|string|max:255',
+        'email'         => 'required|email|max:255',
+        'nomor_telepon' => 'required|string|max:20',
+        'tanggal_lahir' => 'required|date',
+        'alamat'        => 'required|string|max:255',
+        'tanggal_masuk' => 'required|date',
+        'status'        => 'required|string|max:50',
+    ]);
+    $employee = Employee::findOrFail($id);
+    $employee->update($request->only([
+        'nama_lengkap',
+        'email',
+        'nomor_telepon',
+        'tanggal_lahir',
+        'alamat',
+        'tanggal_masuk',
+        'status',
+    ]));
+    return redirect()->route('employees.index');
     }
 
     public function destroy(string $id)
